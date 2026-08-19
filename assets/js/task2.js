85e3056f252cb9230c7d8618b5bc9bd821bcc089
kuchinaevgenia <kucinaevgenia6@gmail.com>
2026-08-19 20:32:25 +0300

    Complete 2.1

---
+++ assets/js/task2.js
@@ -0,0 +1,11 @@
+const arr = [
+  -7, 4, -1, 10, -10, 0, 8, -3, 2, -9, 5, -6, 1, 7, -2, 9, -5, 3, -8, 6,
+];
+
+const evenInArr = [];
+for (let i = 0; i < arr.length; i++) {
+  if (i % 2 === 0) {
+    evenInArr.push(arr[i]);
+  }
+}
+console.log(evenInArr);
