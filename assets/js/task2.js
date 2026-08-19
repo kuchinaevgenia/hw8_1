1c4d4434870573f0df10bf707ef1c50786bd6815
kuchinaevgenia <kucinaevgenia6@gmail.com>
2026-08-19 20:42:14 +0300

    Complete task2

--- assets/js/task2.js
+++ assets/js/task2.js
@@ -15,3 +15,11 @@ for (let i = 0; i < arr.length; i++) {
   dob *= arr[i];
 }
 console.log(dob);
+
+let counter = 0;
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] >= 0) {
+    counter++;
+  }
+}
+console.log(counter);
